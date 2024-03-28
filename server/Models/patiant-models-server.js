5be6b2811d87b25639d271e530e8c15206be348d
ArunaRaviKumar5 <araviku4@my.centennialcollege.ca>
2024-03-28 11:26:27 -0400

    PateintSchema
    PateintSchema

--- server/Models/patiant-models-server.js
+++ server/Models/patiant-models-server.js
@@ -1,1 +1,19 @@
 //Aruna
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const PatientSchema = new Schema({
+
+    firstName:String,
+    lastName:String,
+    age: String,
+    address: String,
+    dateOfBirth: String,
+    HealthCardNumber: String,
+    Gender: String
+
+
+});
+
+module.exports = mongoose.model('Patient',PatientSchema);
